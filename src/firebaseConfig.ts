e5c6a4ed877b70a3421724509ea8822251d90a3e
LahariReddyMuthyala <laharireddy4@gmail.com>
2024-11-08 20:56:40 -0800

    test config

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -14,6 +14,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+console.log(firebaseConfig)
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
